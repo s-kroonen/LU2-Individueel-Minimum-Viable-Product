a13adeeae6ca39d5991ea6053814a4a04844554c
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2025-02-14 12:20:21 +0100

    Add project files.

---
+++ WebApi.api/Repositories/WeatherForecastRepository.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace WebApi.api.Repositories
+{
+    public class WeatherForecastRepository
+    {
+        private readonly string sqlConnectionString;
+
+        public WeatherForecastRepository(string sqlConnectionString)
+        {
+            this.sqlConnectionString = sqlConnectionString;
+        }
+
+        public async Task<WeatherForecast> InsertAsync(WeatherForecast weatherForecast)
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                var environmentId = await sqlConnection.ExecuteAsync("INSERT INTO [WeatherForecast] (Id, TemperatureC, Summary) VALUES (@Id, @TemperatureC, @Summary)", weatherForecast);
+                return weatherForecast;
+            }
+        }
+
+        public async Task<WeatherForecast?> ReadAsync(Guid id)
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                return await sqlConnection.QuerySingleOrDefaultAsync<WeatherForecast>("SELECT * FROM [WeatherForecast] WHERE Id = @Id", new { id });
+            }
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> ReadAsync()
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                return await sqlConnection.QueryAsync<WeatherForecast>("SELECT * FROM [WeatherForecast]");
+            }
+        }
+
+        public async Task UpdateAsync(WeatherForecast environment)
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                await sqlConnection.ExecuteAsync("UPDATE [WeatherForecast] SET " +
+                                                 "TemperatureC = @TemperatureC, " +
+                                                 "Summary = @Summary"
+                                                 , environment);
+
+            }
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                await sqlConnection.ExecuteAsync("DELETE FROM [WeatherForecast] WHERE Id = @Id", new { id });
+            }
+        }
+
+    }
+}
