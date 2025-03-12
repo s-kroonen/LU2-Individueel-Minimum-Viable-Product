6483ffb8dde982576433c9057f8b5a874691ebd2
S.k2004 <stormkroonen@hotmail.nl>
2025-03-12 11:16:05 +0100

    api update

--- WebApi.api/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApi.api.Models;
-
-public class User
-{
-    public string Username { get; set; }
-
-    public string Password { get; set; }
-
-}
--- WebApi.api/Program.cs
+++ WebApi.api/Program.cs
@@ -1,11 +1,11 @@
 using WebApi.api.Repositories;
+using Avans.Identity.Dapper;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var sqlConnectionString = builder.Configuration["SqlConnectionString"];
@@ -14,14 +14,15 @@ var loggerFactory = LoggerFactory.Create(logging => logging.AddConsole());
 var logger = loggerFactory.CreateLogger("Startup");
 logger.LogInformation($"Using SQL Connection String: {sqlConnectionString}");
 
-builder.Services.AddSingleton<ILogger<UserRepository>>(sp =>
-{
-    return loggerFactory.CreateLogger<UserRepository>();
-});
+builder.Services.AddAuthorization();
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>()
+    .AddDapperStores(options =>
+    {
+        options.ConnectionString = sqlConnectionString;
+    });
 
 builder.Services.AddTransient<WeatherForecastRepository, WeatherForecastRepository>(o => new WeatherForecastRepository(sqlConnectionString));
-builder.Services.AddTransient<UserRepository>(sp =>
-    new UserRepository(sqlConnectionString, sp.GetRequiredService<ILogger<UserRepository>>()));
 
 
 var app = builder.Build();
